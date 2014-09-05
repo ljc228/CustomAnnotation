bccc4d49a02590163055a800e7cff2898959a8ee
ljc228 <ljc228@bham.ac.uk>
2014-09-05 16:08:00 +0100

    Added GetNextFilename

--- CustomAnnotation/Logging.xaml.cs
+++ CustomAnnotation/Logging.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.IO;
 
 namespace CustomAnnotation
 {
@@ -78,12 +79,58 @@ namespace CustomAnnotation
             try
             {
                 System.IO.Directory.CreateDirectory("C:\\WOZAnnotationLogs");
-                System.IO.File.WriteAllLines(@"C:\WOZAnnotationLogs\" + Filename.Text + ".txt", lines);
+                string fname = NextAvailableFilename("C:\\WOZAnnotationLogs\\" + Filename.Text + ".txt");
+                System.IO.File.WriteAllLines(@fname, lines);
             }
             catch (Exception exception)
             {
 
             }
+        }
+
+        private static string numberPattern = " ({0})";
+
+        public static string NextAvailableFilename(string path)
+        {
+            // Short-cut if already available
+            if (!File.Exists(path))
+                return path;
+
+            // If path has extension then insert the number pattern just before the extension and return next filename
+            if (System.IO.Path.HasExtension(path))
+                return GetNextFilename(path.Insert(path.LastIndexOf(System.IO.Path.GetExtension(path)), numberPattern));
+
+            // Otherwise just append the pattern to the path and return next filename
+            return GetNextFilename(path + numberPattern);
+        }
+
+        private static string GetNextFilename(string pattern)
+        {
+            string tmp = string.Format(pattern, 1);
+            if (tmp == pattern)
+                throw new ArgumentException("The pattern must include an index place-holder", "pattern");
+
+            if (!File.Exists(tmp))
+                return tmp; // short-circuit if no matches
+
+            int min = 1, max = 2; // min is inclusive, max is exclusive/untested
+
+            while (File.Exists(string.Format(pattern, max)))
+            {
+                min = max;
+                max *= 2;
+            }
+
+            while (max != min + 1)
+            {
+                int pivot = (max + min) / 2;
+                if (File.Exists(string.Format(pattern, pivot)))
+                    min = pivot;
+                else
+                    max = pivot;
+            }
+
+            return string.Format(pattern, max);
         }
 
     }
